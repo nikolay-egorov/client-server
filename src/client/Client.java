a9adbd0fd8f3fa6fbde49ee3992c4ed5f441a651
nikolay-egorov <deathadder98@gmail.com>
2019-02-23 14:58:24 +0300

    Client improvements

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,10 @@
 package client;
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -9,12 +13,14 @@ public class Client {
 
     private String hostServ;
     private Scanner socketInbound;
+    private PrintWriter socketOut = null;
 
     public Client(String host, int port) {
         hostServ=host;
         try {
             Socket socket = new Socket(host, port);
             socketInbound = new Scanner(socket.getInputStream());
+            socketOut = new PrintWriter(socket.getOutputStream(), true);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -27,6 +33,32 @@ public class Client {
 
     public void getImageList(){
         int length = Integer.parseInt(socketInbound.nextLine());
-        
+
+    }
+
+    public void downloadFromServer(String fileName){
+        socketOut.println("requesting::" + fileName);
+        try {
+            System.out.println("Opening the new socket for connection...");
+            ServerSocket server = new ServerSocket(3001);
+            Socket socket = server.accept();
+            System.out.println(" Connection with server established.\nRetrieving input stream...");
+            InputStream inStream = socket.getInputStream();
+            DataInputStream dis = new DataInputStream(inStream);
+
+            int dataLength = dis.readInt();
+            byte[] data = new byte[dataLength];
+            dis.readFully(data);
+            dis.close();
+            inStream.close();
+            System.out.println(" Finished receiving input stream.\nConverting to file...");
+
+
+
+            //TODO conversation
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
