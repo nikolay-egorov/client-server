a29c051225148bc84ce494128ba17ebf94b81150
nikolay-egorov <deathadder98@gmail.com>
2019-02-28 14:45:00 +0300

    Major Update

--- src/server/QueryHandler.java
+++ src/server/QueryHandler.java
@@ -1,12 +1,18 @@
 package server;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class QueryHandler implements Runnable{
@@ -40,6 +46,23 @@ public class QueryHandler implements Runnable{
     }
 
     private void handleQuery() {
+        String fileName = "";
+        // Retrieve filename from terminal query.
+        int separatorPos = this.message.lastIndexOf("::");
+        if ((separatorPos > 0) && this.message.contains("::")) {
+            if (!this.message.endsWith("::")) {
+                fileName = this.message.substring(separatorPos + 2);
+            }
+        }
+
+        if (this.message.startsWith("requesting::"))
+        {	SendFileToUser(fileName); }
+//        else if (this.message.startsWith("sending::"))
+//        {	DownloadFromUser(fileName); }
+        else if (this.message.startsWith("requestlist::"))
+        {	SendImageList(); }
+        else
+        {	Send("Invalid query: " + this.message); }
 
     }
 
@@ -48,11 +71,59 @@ public class QueryHandler implements Runnable{
     private void ReadImagesFromFile()
     {
 
+        System.out.println("Indexing own base...");
+        File dir = new File(this.dirPath + "/images");
+        this.fileList = dir.listFiles();
+        this.imageList = new ArrayList<String>(this.fileList.length);
+        for (File file : this.fileList) {
+            if (file.isFile()) {
+                System.out.println(" adding " + file.getName());
+                this.imageList.add(file.getName());
+            }
+        }
+        System.out.println();
 
     }
 
     public void SendFileToUser(String fileName)
     {
+        try
+        {
+            int dotLocation = fileName.lastIndexOf(".");
+            String fileType = "";
+            if (dotLocation > 0)
+            {	fileType = fileName.substring(dotLocation+1);
+            }
+            System.out.println("Detected file type: " + fileType);
+            System.out.println("Analyzing file: /images/" + fileName);
+            BufferedImage img = ImageIO.read(new File(this.dirPath + "/images/" + fileName));
+            System.out.println("Converting image to byte array output stream.");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(img, fileType, baos);
+            baos.flush();
+            byte[] bytes = baos.toByteArray();
+            baos.close();
+            System.out.println("Byte array with " + bytes.length + " length created.");
+
+            System.out.println("Opening new socket to connect to user.");
+            Socket soc = new Socket(this.clientAddress, 4000);
+            System.out.println("Opening stream with user.");
+            OutputStream outStream = soc.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(outStream);
+
+            System.out.println("Writing to user stream.");
+            dos.writeInt(bytes.length);
+            dos.write(bytes, 0, bytes.length);
+            System.out.println("Closing stream and socket.");
+            dos.close();
+            outStream.close();
+            soc.close();
+            writeToLog("image sent", fileName);
+        }
+
+        catch (Exception e)
+        {	e.printStackTrace();
+        }
 
     }
 
@@ -60,8 +131,47 @@ public class QueryHandler implements Runnable{
 
     public void SendImageList()
     {
-        
+        ReadImagesFromFile();
+        Send(Integer.toString(imageList.size()));
+        for (String anImageList : this.imageList) {
+            Send(anImageList);
+        }
+        Send("stop::");
     }
+
+    private void writeToLog(String functionRequested, String filename)
+    {
+        File log = new File("servlog.txt");
+
+        if (!log.exists())
+        {
+            try
+            {	PrintWriter writer = new PrintWriter("servlog.txt", "UTF-8");
+                writer.write("");
+                writer.close();
+            }
+            catch (Exception e)
+            {	e.printStackTrace();
+            }
+        }
+        // Create a new Date object and read date and time from it as strings.
+        Date currentDateTime = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        String request = functionRequested + " " + filename;
+        String date = dateFormat.format(currentDateTime);
+        String time = timeFormat.format(currentDateTime);
+        String toLog = date + " : " + time + " : " + this.clientAddress + " : " + request + "\n";
+        // Write to serverlog.txt
+        try
+        {	Files.write((Paths.get("servlog.txt")), toLog.getBytes(), StandardOpenOption.APPEND);
+            System.out.println("Event has been logged.");
+        }
+        catch (IOException e)
+        {	e.printStackTrace();
+        }
+    }
+
 
 
 
