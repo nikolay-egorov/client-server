076a9fb77586288e12deca021baf2daa5e51b209
nikolay-egorov <deathadder98@gmail.com>
2019-03-11 22:35:25 +0300

    WallChanger si done

--- src/client/WallChanger.java
+++ src/client/WallChanger.java
@@ -1,8 +1,12 @@
 package client;
 
+
+import com.sun.jna.Library;
+import com.sun.jna.win32.W32APIOptions;
+import com.sun.jna.Native;
 import java.io.IOException;
 
-class WallChanger {
+public class WallChanger {
 
 
     public void setMac(String path) throws IOException {
@@ -27,22 +31,26 @@ class WallChanger {
      */
     public static native int SystemParametersInfo(int uiAction, int uiParam, String pvParam, int fWinIni);
 
-    static {
-        System.loadLibrary("user32");
+
+    public static interface User32 extends Library {
+        User32 INSTANCE = (User32) Native.loadLibrary("user32",User32.class, W32APIOptions.DEFAULT_OPTIONS);
+        boolean SystemParametersInfo (int one, int two, String s ,int three);
     }
+
+
 
     /**
      * For Wallpaper changer which is uiAction == 20 and uiParam is obligatory == 0
      * @param path - path to the wallpaper file
      * @return
      */
-    public int Change(String path) throws IOException {
+    public void changeWallpaper(String path) throws IOException {
 
         if (OsDefiner.isWindows())
-            return SystemParametersInfo(20, 0, path, 0);
+             User32.INSTANCE.SystemParametersInfo(0x0014, 0, path, 1);
         else if (OsDefiner.isUnix())
             setMac(path);
-        return 0;
+
     }
 
 
