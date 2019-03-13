d859105b2cf3f4f3f47eccd63d735468cff8f68a
nikolay-egorov <deathadder98@gmail.com>
2019-03-13 22:31:33 +0300

    Server is now with thread pool

--- src/server/ServerBase.java
+++ src/server/ServerBase.java
@@ -2,22 +2,48 @@ package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class ServerBase {
 
     private ServerSocket serverSocket = null;
+    private Integer threadLimit = null;
 
-
-    public ServerBase(int port) {
+    public ServerBase(int port, int threadLimit) {
+        this.threadLimit= threadLimit;
         try {
             serverSocket= new ServerSocket(port);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Thread sclThread = new Thread(new ServerConnectLoop());
+        sclThread.start();
+    }
+
+    public class ServerConnectLoop implements Runnable
+    {
+        ThreadPoolExecutor pool = (ThreadPoolExecutor) Executors.newFixedThreadPool(threadLimit);
+
+        public void run()
+        {
+            try
+            {	while (true)
+            {	Socket clientSocket = serverSocket.accept();
+                QueryHandler q = new QueryHandler(clientSocket);
+                pool.execute(q);
+                System.out.println("[SERVER] Client connected. " + pool.getActiveCount() + " client(s) connected.");
+            }
+            }
+            catch (IOException e)
+            {	e.printStackTrace();
+            }
+        }
     }
 
 
     public static void main(String[] args) {
-        ServerBase s= new ServerBase(3221);
+        ServerBase s= new ServerBase(6000,5);
     }
 }
